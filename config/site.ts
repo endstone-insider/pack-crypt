3d4cf20f832fe8ab03956fba130b17bbdebf2444
Vincent <magicdroidx@gmail.com>
2025-05-06 21:46:04 +0100

    feat: update site configuration for PackCrypt branding

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + HeroUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "PackCrypt - Simple, fast, secure client-side file encryption.",
+  description: "Simple, fast, secure client-side file encryption.",
   links: {
     github: "https://github.com/EndstoneMC/pack-crypt",
     discord: "https://discord.gg/xxgPuc2XN9",
